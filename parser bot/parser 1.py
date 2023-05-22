ce8189fec0c24dda05af75b4e274a83dcfad8d9d
Владислав <vlad_magasumov@mail.ru>
2023-05-22 14:01:57 +0300

    ggg

--- parser bot/parser 1.py
+++ parser bot/parser 1.py
@@ -6,10 +6,10 @@ import json
 
 # url = 'https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
 #
-# headers = {
-#     'Accept': '*/*' ,
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0'
-# }
+headers = {
+    'Accept': '*/*' ,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0'
+}
 #
 # req = requests.get(url, headers=headers)
 # src = req.text
@@ -18,26 +18,39 @@ import json
 # with open('index.html', 'w', encoding='utf-8') as file:
 #     file.write(src)
 
-with open('index.html', encoding='utf-8') as file:
-    src = file.read()
+# with open('index.html', encoding="utf 8") as file:
+#     src = file.read()
+#
+#
+# soup = BeautifulSoup(src, "lxml")
+# all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+#
+# all_categories_dict = {}
+# for item in all_products_hrefs:
+#     item_text = item.text
+#     item_href = "https://health-diet.ru" + item.get("href")
 
 
-soup = BeautifulSoup(src, "lxml")
-all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+#     all_categories_dict[item_text] = item_href
+#
+# with open("all_categories_dict.json", "w") as file:
+#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
 
-all_categories_dict = {}
-for item in all_products_hrefs:
-    item_text = item.text
-    item_herf = "https://health-diet.ru" + item.get("href")
-    # print(f"{item_text}: {item_herf}")
-    # print(item)
+with open("all_categories_dict.json") as file:
+    all_categories = json.load(file)
+count = 0
+for category_name, category_href in all_categories.items():
 
-    all_categories_dict[item_text] = item_href
+    if count == 0:
+        rep = [",", "-", " ", "'"]
+        for item in rep:
+            if item in category_name:
+                category_name = category_name.replace(item,"_")
 
-with open("all_categories_dict.json", "w") as file:
-    json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
-    # lgndrbnd
-<<<<<<< HEAD
-ghyhchcbxj
-=======
->>>>>>> origin/master
+        req = requests.get(url=category_href, headers=headers)
+        src = req.text
+
+        with open(f"data/{count}_{category_name}.html", "w", encoding='utf-8') as file:
+            file.write(src)
+
+    count += 1
